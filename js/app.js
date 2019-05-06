09917246536a34f6dcbae25593492f2152f75749
Cryptopurr Columbu <cryptopurrcolumbus@gmail.com>
2019-05-06 22:34:03 +0530

    Add movement feature for player
    Signed-off-by: Cryptopurr Columbu <cryptopurrcolumbus@gmail.com>

--- js/app.js
+++ js/app.js
@@ -23,41 +23,43 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
-var Player = function(x, y) {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/char-boy.png';
-    this.x = x;
-    this.y = y;
-};
+// Our player object
+var player = {
+    sprite: 'images/char-boy.png',
+    x: 205,
+    y: 450,
+    render: function() {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    },
+    handleInput: function(direction) {
+        if (direction == 'up') {
+            this.y -= 50;
+            if (this.y <= -10) {
+                this.y = -10;
+            }
+        } else if (direction == 'down') {
+            this.y += 50;
+            if (this.y >= 450) {
+                this.y = 450;
+            }
+        } else if (direction == 'right') {
+            this.x += 50;
+            if (this.x >= 400) {
+                this.x = 400;
+            }
+        } else if (direction == 'left') {
+            this.x -= 50;
+            if (this.x <= 0) {
+                this.x = 0;
+            }
+        }
+    },
+    update: function(dt) {
+        return null;
+    }
+}
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Player.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-};
-
-// Draw the enemy on the screen, required method for game
-Player.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
-
-Player.prototype.handleInput = function(direction) {
- // write shit here
- console.log(direction);
-};
-
-
-// Now instantiate your objects.
-player =  new Player(205, 450);
 
 // Place all enemy objects in an array called allEnemies
 allEnemies = [ 
