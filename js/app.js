5e107bc36b726e9dd30ab4e331493c8dd79d1b8b
Cryptopurr Columbu <cryptopurrcolumbus@gmail.com>
2019-05-08 00:35:41 +0530

    Add winning logic, actions and game object
    Signed-off-by: Cryptopurr Columbu <cryptopurrcolumbus@gmail.com>

--- js/app.js
+++ js/app.js
@@ -19,10 +19,9 @@ Enemy.prototype.update = function(dt) {
     // all computers.
     this.x += this.speed * dt;
     if (this.x > 550) {
-        var properties = getRandomEnemyProperties();
-        this.x = properties.x;
-        this.y = properties.y;
-        this.speed = properties.speed;
+        // the enemy is beyond the screen, get him back
+        // in the game by resetting the properties
+        this.resetToRandomProperties();
     }
 };
 
@@ -31,6 +30,55 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
+// Reset to random set of properties
+Enemy.prototype.resetToRandomProperties = function() {
+    var properties = getRandomEnemyProperties();
+    this.x = properties.x;
+    this.y = properties.y;
+    this.speed = properties.speed;
+}
+
+// Game level actions defined here. Ex: The handling of winning of
+// the game doesn't really belong to the player or the enemy
+var game = {
+    playerWon: function() {
+        // Hide all enemies
+        allEnemies.forEach(function(enemy) {
+            enemy.speed = 0;
+            enemy.x = -100;
+        });
+        // add some html to tell the user that he has won
+        // The winning para to be displayed
+        var para = document.createElement('p');
+        para.className = 'winning-para';
+        var textNode = document.createTextNode("You won!");
+        para.appendChild(textNode);
+
+        // Reset game option
+        var resetButton = document.createElement("button");
+        resetButton.innerHTML = "Reset Game";
+        document.body.append(para);
+        document.body.append(resetButton);
+
+        // Event listener for reset game button
+        resetButton.addEventListener(
+            'click', this.handleResetGameButton.bind(null, para, resetButton))
+    },
+    handleResetGameButton: function(para, resetButton) {
+        // "this" refers to HTMLButtonElement and not the player object
+        // bring the player to it's original position
+        player.x = 205;
+        player.y = 400;
+
+        // make the enemies move from random positions
+        allEnemies.forEach(function(enemy) {
+            enemy.resetToRandomProperties();
+        });
+        // remove the html elements from dom
+        document.body.removeChild(para);
+        document.body.removeChild(resetButton);
+    },
+}
 
 // Our player object
 var player = {
@@ -43,21 +91,27 @@ var player = {
     handleInput: function(direction) {
         if (direction == 'up') {
             this.y -= 50;
+            // The player has reached the top
+            // and has won the game thus.
             if (this.y <= -10) {
                 this.y = -10;
+                game.playerWon();
             }
         } else if (direction == 'down') {
             this.y += 50;
+            // The player has reached the bottom
             if (this.y >= 450) {
                 this.y = 450;
             }
         } else if (direction == 'right') {
             this.x += 50;
+            // The player has reached the rightmost corner
             if (this.x >= 400) {
                 this.x = 400;
             }
         } else if (direction == 'left') {
             this.x -= 50;
+            // The player has reached the leftmost corner
             if (this.x <= 0) {
                 this.x = 0;
             }
@@ -65,7 +119,7 @@ var player = {
     },
     update: function(dt) {
         // noop
-    }
+    },
 }
 
 
@@ -74,9 +128,10 @@ function getRandomNumber(min, max) {
     return Math.random() * (max - min) + min;
 }
 
-
+// Useful for randomizing the enemies properties
+// This way, they are unpredictable
 function getRandomEnemyProperties() {
-    var enemy_x = getRandomNumber(-150, -10);
+    var enemy_x = getRandomNumber(-150, -120);
     var enemy_y = getRandomNumber(50, 240);
     var enemy_speed = getRandomNumber(20, 200);
     return {
@@ -88,7 +143,7 @@ function getRandomEnemyProperties() {
 
 // Our array of enemy objects
 allEnemies = [];
-for (var i = 0; i <= 2; i++) {
+for (var i = 0; i <= 10; i++) {
     var properties = getRandomEnemyProperties();
     var enemy_x = properties.x;
     var enemy_y = properties.y;
